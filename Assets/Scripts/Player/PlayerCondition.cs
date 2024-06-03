2d8d86855eea47b11c228e60caeab8cc57f86f2d
Besin <0321sjj@naver.com>
2024-06-03 20:19:21 +0900

    Chore: 배고픔, 목마름 구현ing 임시

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -8,6 +8,8 @@ public class PlayerCondition : MonoBehaviour
 
     private Condition hp { get { return conditions.hp; } }
     private Condition sp { get { return conditions.sp; } }
+    private Condition ep { get { return conditions.ep; } }
+    private Condition wp { get { return conditions.wp; } }
 
     private void Update()
     {
@@ -19,6 +21,26 @@ public class PlayerCondition : MonoBehaviour
         if (sp.currentValue > value)
         {
             sp.Down(value);
+            return true;
+        }
+        return false;
+    }
+
+    public bool UseEp(float value)
+    {
+        if (ep.currentValue > value)
+        {
+            //ep.Down(value);
+            return true;
+        }
+        return false;
+    }
+
+    public bool UseWp(float value)
+    {
+        if (wp.currentValue > value)
+        {
+            //wp.Down(value);
             return true;
         }
         return false;
--- Assets/Scripts/UI/UICondition.cs
+++ Assets/Scripts/UI/UICondition.cs
@@ -6,6 +6,8 @@ public class UICondition : MonoBehaviour
 {
     public Condition hp;
     public Condition sp;
+    public Condition ep;
+    public Condition wp;
 
     private void Start()
     {
