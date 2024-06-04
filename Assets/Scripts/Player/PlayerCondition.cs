c0a744c352b55a3373b543ed90b26b336939a7fe
Besin <0321sjj@naver.com>
2024-06-04 17:47:37 +0900

    chore : 배고픔, 목마름 구현 및 플레이어 상태창 ui 수정

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCondition : MonoBehaviour
@@ -11,9 +12,17 @@ public class PlayerCondition : MonoBehaviour
     private Condition ep { get { return conditions.ep; } }
     private Condition wp { get { return conditions.wp; } }
 
+    public float noEpHealthDecay;
+
     private void Update()
     {
+        ep.Up(ep.decayValue * Time.deltaTime);
+        wp.Up(wp.decayValue * Time.deltaTime);
 
+        if(ep.currentValue <= 0f || wp.currentValue <= 0f)
+        {
+            hp.Down(noEpHealthDecay * Time.deltaTime);
+        }
     }
 
     public bool UseSp(float value)
@@ -26,25 +35,25 @@ public class PlayerCondition : MonoBehaviour
         return false;
     }
 
-    public bool UseEp(float value)
-    {
-        if (ep.currentValue > value)
-        {
-            //ep.Down(value);
-            return true;
-        }
-        return false;
-    }
+    //public bool UseEp(float value)
+    //{
+    //    if (ep.currentValue > value)
+    //    {
+    //        ep.Down(value);
+    //        return true;
+    //    }
+    //    return false;
+    //}
 
-    public bool UseWp(float value)
-    {
-        if (wp.currentValue > value)
-        {
-            //wp.Down(value);
-            return true;
-        }
-        return false;
-    }
+    //public bool UseWp(float value)
+    //{
+    //    if (wp.currentValue > value)
+    //    {
+    //        wp.Down(value);
+    //        return true;
+    //    }
+    //    return false;
+    //}
 
     public void Heal(float value)
     {
@@ -54,5 +63,15 @@ public class PlayerCondition : MonoBehaviour
     public void Restore(float value)
     {
         sp.Up(value);
+    }
+
+    public void Eat(float value)
+    {
+        ep.Up(value);
+    }
+
+    public void Drink(float value)
+    {
+        wp.Up(value);
     }
 }
